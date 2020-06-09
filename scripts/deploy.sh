da5b1f1541e92dd2daef2cf812d2f9caa11aa0d6
Pavel Dronenko <p.dronenko@wellyes.ru>
2020-06-09 08:15:36 +0300

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+docker rm -f travel_blog_api
+docker image prune
+docker volume prune
+docker build -t travel_blog_api .
 sudo docker-compose -f ./docker-compose.yml up --force-recreate -d
